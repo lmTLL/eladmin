374576fcf650a1e11919ae95479132af51c85832
pangxianwei <2442741792@qq.com>
2019-09-10 16:16:35 +0800

    2019/09/10  系统增加站外下单

--- eladmin-system/src/main/java/me/zhengjie/modules/system/rest/AsinInfoController.java
+++ eladmin-system/src/main/java/me/zhengjie/modules/system/rest/AsinInfoController.java
@@ -1,5 +1,6 @@
 package me.zhengjie.modules.system.rest;
 
+import com.google.gson.Gson;
 import me.zhengjie.aop.log.Log;
 import me.zhengjie.modules.system.domain.AsinInfo;
 import me.zhengjie.modules.system.domain.Role;
@@ -9,6 +10,8 @@ import me.zhengjie.modules.system.service.AsinInfoService;
 import me.zhengjie.modules.system.service.dto.AsinInfoQueryCriteria;
 import me.zhengjie.modules.system.service.impl.AmzWebCrawlerServiceImpl;
 import me.zhengjie.utils.SecurityUtils;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -20,12 +23,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Set;
-
-
+import java.util.*;
 /**
 * @author groot
 * @date 2019-07-22
@@ -39,6 +37,8 @@ public class AsinInfoController {
     private AsinInfoService asinInfoService;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private WechatController wechatController;
     @Log("查询AsinInfo")
     @GetMapping(value = "/asinInfo")
     @PreAuthorize("hasAnyRole('ADMIN','ASININFO_ALL','ASININFO_SELECT')")
@@ -68,6 +68,14 @@ public class AsinInfoController {
         Timestamp timestamp=new Timestamp(date.getTime());
         resources.setStartDate(timestamp);
         resources.setUpdateDate(timestamp);
+        try {
+            AsinInfo aSin = nicknaeHead(resources.getOpenId());
+            resources.setNickName(aSin.getNickName());
+            resources.setHeadImgurl(aSin.getHeadImgurl());
+        }catch (Exception e){
+            System.out.println("找不到昵称，头像URL");
+            System.out.println(e);
+        }
         ResponseEntity returning = new ResponseEntity(asinInfoService.create(resources),HttpStatus.CREATED);
         runReptile(resources);
         return returning;
@@ -92,7 +100,46 @@ public class AsinInfoController {
         asinInfoService.delete(id);
         return new ResponseEntity(HttpStatus.OK);
     }
+    /**
+     *  利用openId获取用户昵称和头像URI
+     * @param openid
+     * @return
+     * @throws Exception
+     */
+    public AsinInfo nicknaeHead(String openid) throws Exception {
 
+        String access_tocken = wechatController.getAccessToken();
+        System.out.println("==================token:"+access_tocken);
+        String url = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=" + access_tocken + "&openid=" + openid;
+        HttpClient client = new HttpClient();
+        client.getParams().setContentCharset("UTF-8");
+        GetMethod httpGet = new GetMethod(url);
+        try {
+            client.executeMethod(httpGet);
+            String response = httpGet.getResponseBodyAsString();
+            Gson gson = new Gson();
+            Map<String, Object> wechatUserInfoMap = gson.fromJson(response, Map.class);
+            AsinInfo asinInfo =new AsinInfo();
+            if (wechatUserInfoMap.get("nickname") != null) {
+                asinInfo.setNickName(wechatUserInfoMap.get("nickname").toString());
+            }
+            if (wechatUserInfoMap.get("headimgurl") != null){
+                asinInfo.setHeadImgurl(wechatUserInfoMap.get("headimgurl").toString());
+            }
+            return asinInfo;
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            httpGet.releaseConnection();
+        }
+    }
+
+    /**
+     * 爬虫方法
+     * @param resources 用户类
+     * @throws InterruptedException
+     * @throws IOException
+     */
     public void runReptile(AsinInfo resources) throws InterruptedException, IOException {
         new Thread(new Runnable() {
             @Override
